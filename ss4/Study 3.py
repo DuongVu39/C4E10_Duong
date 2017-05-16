446cf9c2ad2f5e3c27cd0a5eb3e5b3b2ce4ccc6b
DuongVu39 <dqvu@umail.iu.edu>
2017-05-16 12:07:22 -0400

    Add files via upload
    Bạn Dương nộp bài

---
+++ ss4/Study 3.py
@@ -0,0 +1,24 @@
+"""Alice_words"""
+file = open('alice_words.txt','w')
+text = input('Alice’s Adventures in Wonderland:').lower()
+word_count={}
+from collections import Counter
+word_count = Counter (text.split())
+
+var = ('{:15}{:7}'.format('Word','Count'))
+
+var1= []
+
+for (word,occurance) in sorted(word_count.items()):
+    x = ({'{:15}{:3}'.format(word,occurance)})
+    var1.append(x)
+
+var1.sort()
+
+with open('alice_words.txt', 'a') as out:
+    out.write(var + '\n')
+    out.write('-' *22)
+    out.write('\n')
+    for item in var1:
+        out.write("{}\n".format(item))
+
